34431e1845e5348e88c5f945919a0a71945e5f9f
Junior Muteba <junior.muteba10@gmail.com>
2025-08-23 11:31:57 +0200

    Ajout du post-install-cmd et modification de vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/junior.css'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        emptyOutDir: true,
+    },
 });
