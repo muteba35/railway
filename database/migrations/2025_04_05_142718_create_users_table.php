18962bf258eea8ce16f7d9834f870e2d754c2796
Junior Muteba <junior.muteba10@gmail.com>
2025-08-23 15:56:32 +0200

    Add phone column to users table

--- database/migrations/2025_04_05_142718_create_users_table.php
+++ database/migrations/2025_04_05_142718_create_users_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
         $table->id();
-    $table->string('nom');
-    $table->string('postnom')->nullable();
+    $table->string('name');
     $table->string('email')->unique();
     $table->string('phone')->nullable();
 
